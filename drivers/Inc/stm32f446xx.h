5b59ae61adfe528b94d292cc22de6d4a264b6455
banabina <banabina0012@gmail.com>
2021-01-04 09:17:44 +0900

    feat: Define gpio driver API

--- drivers/Inc/stm32f446xx.h
+++ drivers/Inc/stm32f446xx.h
@@ -235,5 +235,12 @@ typedef struct
  */
 #define SYSCFG_PCLK_DI()	( RCC->APB2ENR &= ~( 1 << 14 ) )
 
+// some generic macors
+#define ENABLE 				1
+#define DISABLE 			0
+#define SET					ENABLE
+#define RESET				DISABLE
+#define GPIO_PIN_SET		SET
+#define GPIO_PIN_RESET		RESET
 
 #endif /* INC_STM32F446XX_H_ */
---
+++ drivers/Src/stm32f446xx_gpio_driver.c
@@ -0,0 +1,9 @@
+/*
+ * stm32f446xx_gpio_driver.c
+ *
+ *  Created on: 2021. 1. 3.
+ *      Author: JKwan
+ */
+
+
+#include "stm32f446xx_gpio_driver.h"
