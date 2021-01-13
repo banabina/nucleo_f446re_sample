467272abdf0ee0dbc6dd9342c7cb0190d9ab369a
banabina <banabina0012@gmail.com>
2021-01-13 11:15:49 +0900

    fix : fix spi dirver header file

--- drivers/Inc/stm32f446xx_spi_driver.h
+++ drivers/Inc/stm32f446xx_spi_driver.h
@@ -8,6 +8,27 @@
 #ifndef INC_STM32F446XX_SPI_DRIVER_H_
 #define INC_STM32F446XX_SPI_DRIVER_H_
 
+#include"stm32f446xx.h"
+
+typedef struct
+{
+	uint8_t SPI_DeviceMode;
+	uint8_t SPI_BusConfig;
+	uint8_t SPI_SclkSpeed;
+	uint8_t SPI_DFF;
+	uint8_t SPI_CPOL;
+	uint8_t SPI_CPHA;
+	uint8_t SPI_SSM;
+}SPI_Config_t;
+
+/*
+ * Handle structure for SPIx peripheral
+ */
+typedef struct
+{
+	SPI_RegDef_t	*pSPIx;		/* !< This holds the base address of SPIx(x:0,1,2) peripheral */
+	SPI_Config_t	SPIConfig;
+}SPI_Handle_t;
 
 
 #endif /* INC_STM32F446XX_SPI_DRIVER_H_ */
--- drivers/Src/stm32f446xx_spi_driver.c
+++ drivers/Src/stm32f446xx_spi_driver.c
@@ -9,24 +9,3 @@
  * Configuration structure for SPIx peripheral
  */
 
-#include"stm32f446xx.h"
-
-typedef struct
-{
-	uint8_t SPI_DeviceMode;
-	uint8_t SPI_BusConfig;
-	uint8_t SPI_SclkSpeed;
-	uint8_t SPI_DFF;
-	uint8_t SPI_CPOL;
-	uint8_t SPI_CPHA;
-	uint8_t SPI_SSM;
-}SPI_Config_t;
-
-/*
- * Handle structure for SPIx peripheral
- */
-typedef struct
-{
-	SPI_RegDef_t	*pSPIx;
-	SPI_Config_t	SPIConfig;
-}SPI_Handle_t;
