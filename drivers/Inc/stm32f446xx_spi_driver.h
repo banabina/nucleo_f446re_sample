f940853577166a808d346a48d3559413f1011b2e
banabina <banabina0012@gmail.com>
2021-01-13 15:05:53 +0900

    feat : Add SPI API prototype

--- drivers/Inc/stm32f446xx_spi_driver.h
+++ drivers/Inc/stm32f446xx_spi_driver.h
@@ -30,5 +30,38 @@ typedef struct
 	SPI_Config_t	SPIConfig;
 }SPI_Handle_t;
 
+/*************************************************************************************
+ * 									APIs supported by this driver
+ * 		For more information about the APIs check the function definitions
+ *************************************************************************************/
+/*
+ * Peripheral Clock setup
+ */
+void SPI_PeripheralClockControl(SPI_RegDef_t* pSPIx, uint8_t EnorDi);
+
+/*
+ * Init and De-init
+ */
+void SPI_Init(SPI_Handle_t* pSPIHandle);
+void SPI_DeInit(SPI_RegDef_t* pSPIx);
+
+
+/*
+ * Data send and Receive
+ */
+void SPI_SendData(SPI_RegDef_t* pSPIx, uint8_t* pTxBuffer, uint32_t Len);
+void SPI_ReceiveData(SPI_RegDef_t* pSPIx, uint8_t* pRxBuffer, uint32_t Len);
+
+/*
+ * IRQ Configuration ans ISR handling
+ */
+void SPI_IRQInterruptConfig(uint8_t IRQNumber, uint8_t EnorDi);
+void SPI_IRQPriorityConfig(uint8_t IRQNumber, uint8_t IRQPriority);
+void SPI_IRQHandling(SPI_Handle_t *pHandle);
+
+
+/*
+ * Other Peripheral Control APIs
+ */
 
 #endif /* INC_STM32F446XX_SPI_DRIVER_H_ */
--- drivers/Src/stm32f446xx_spi_driver.c
+++ drivers/Src/stm32f446xx_spi_driver.c
@@ -4,8 +4,156 @@
  *  Created on: 2021. 1. 13.
  *      Author: new_challenger
  */
+#include "stm32f446xx_spi_driver.h"
 
 /*
- * Configuration structure for SPIx peripheral
+ * Peripheral Clock setup
  */
+/*********************************************************************
+ * @fn				- SPI_PeripheralClockControl
+ *
+ * @ brief			- This function enables or disables peripheral clock for the given GPIO port
+ *
+ * @param[in]		- base address of the spi peripheral
+ * @param[in] 		- ENABLE or DISABLE macros
+ *
+ * @return			- none
+ *
+   @Note			- none
+ */
+void SPI_PeripheralClockControl(SPI_RegDef_t* pSPIx, uint8_t EnorDi)
+{
+
+}
+
+/*
+ * Init and De-init
+ */
+/*********************************************************************
+ * @fn				- SPI_Init
+ *
+ * @ brief			- This function enables or disables peripheral clock for the given GPIO port
+ *
+ * @param[in]		- base address of the spi peripheral
+ * @param[in] 		- ENABLE or DISABLE macros
+ *
+ * @return			- none
+ *
+   @Note			- none
+ */
+void SPI_Init(SPI_Handle_t* pSPIHandle)
+{
+
+}
+
+/*********************************************************************
+ * @fn				- SPI_DeInit
+ *
+ * @ brief			- This function enables or disables peripheral clock for the given GPIO port
+ *
+ * @param[in]		- base address of the spi peripheral
+ * @param[in] 		- ENABLE or DISABLE macros
+ *
+ * @return			- none
+ *
+   @Note			- none
+ */
+void SPI_DeInit(SPI_RegDef_t* pSPIx)
+{
+
+}
+
+
+/*
+ * Data send and Receive
+ */
+/*********************************************************************
+ * @fn				- SPI_DeInit
+ *
+ * @ brief			- This function enables or disables peripheral clock for the given GPIO port
+ *
+ * @param[in]		- base address of the spi peripheral
+ * @param[in] 		- ENABLE or DISABLE macros
+ *
+ * @return			- none
+ *
+   @Note			- none
+ */
+void SPI_SendData(SPI_RegDef_t* pSPIx, uint8_t* pTxBuffer, uint32_t Len)
+{
+
+}
+
+
+/*********************************************************************
+ * @fn				- SPI_DeInit
+ *
+ * @ brief			- This function enables or disables peripheral clock for the given GPIO port
+ *
+ * @param[in]		- base address of the spi peripheral
+ * @param[in] 		- ENABLE or DISABLE macros
+ *
+ * @return			- none
+ *
+   @Note			- none
+ */
+void SPI_ReceiveData(SPI_RegDef_t* pSPIx, uint8_t* pRxBuffer, uint32_t Len)
+{
+
+}
+
+/*
+ * IRQ Configuration ans ISR handling
+ */
+
+/*********************************************************************
+ * @fn				- SPI_DeInit
+ *
+ * @ brief			- This function enables or disables peripheral clock for the given GPIO port
+ *
+ * @param[in]		- base address of the spi peripheral
+ * @param[in] 		- ENABLE or DISABLE macros
+ *
+ * @return			- none
+ *
+   @Note			- none
+ */
+void SPI_IRQInterruptConfig(uint8_t IRQNumber, uint8_t EnorDi)
+{
+
+}
+
+/*********************************************************************
+ * @fn				- SPI_DeInit
+ *
+ * @ brief			- This function enables or disables peripheral clock for the given GPIO port
+ *
+ * @param[in]		- base address of the spi peripheral
+ * @param[in] 		- ENABLE or DISABLE macros
+ *
+ * @return			- none
+ *
+   @Note			- none
+ */
+void SPI_IRQPriorityConfig(uint8_t IRQNumber, uint8_t IRQPriority)
+{
+
+}
+
+/*********************************************************************
+ * @fn				- SPI_DeInit
+ *
+ * @ brief			- This function enables or disables peripheral clock for the given GPIO port
+ *
+ * @param[in]		- base address of the spi peripheral
+ * @param[in] 		- ENABLE or DISABLE macros
+ *
+ * @return			- none
+ *
+   @Note			- none
+ */
+void SPI_IRQHandling(SPI_Handle_t *pHandle)
+{
+
+}
 
